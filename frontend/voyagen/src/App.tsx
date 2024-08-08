e48060dd86754d84e8ccece2ed56881c843bdcbe
brian-rd <theblockycharacter@gmail.com>
2024-08-08 20:31:48 +0100

    setting up react router

--- frontend/voyagen/src/App.tsx
+++ frontend/voyagen/src/App.tsx
@@ -1,7 +1,35 @@
-export default function App() {
+import React, { useEffect } from 'react';
+import {
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import './css/style.css';
+
+// Import pages
+import Landing from './pages/Landing';
+
+function App() {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html');
+    if (htmlElement) {
+      htmlElement.style.scrollBehavior = 'auto';
+      window.scroll({ top: 0 });
+      htmlElement.style.scrollBehavior = '';
+    }
+  }, [location.pathname]); // triggered on route change
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </>
+  );
 }
+
+export default App;
